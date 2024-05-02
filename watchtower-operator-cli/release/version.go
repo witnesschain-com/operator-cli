b5548e666b91a3561b540bf5dca6575e5af9a6fd
Aditya K <adityaangadi96@gmail.com>
2024-05-02 11:08:39 +0530

    feature/12 add secure keys feature (#16)
    * feature : Using secure keys
    
    * feature : Adding detailed description in README
    
    * feature : Fixing registered operator check
    
    * fix : fixing the merge conflicts
    
    ---------
    
    Signed-off-by: Aditya <aditya@witnesschain.com>

--- watchtower-operator-cli/release/version.go
+++
@@ -1,5 +0,0 @@
-package release
-
-// var VERSION = strings.Replace(`
-// UNKNOWN
-// `,"\n","",-1)
---
+++ watchtower-operator/release/version.go
@@ -0,0 +1,9 @@
+package release
+
+import "strings"
+
+var VERSION = strings.Replace(`
+feat/25-give-option-to-the-user-to-use-encrypted-keys-or-not
+-
+1713851371
+`,"\n","",-1)
