5ee9d63057b4c6f096c9d19abaf41b3c67fba098
Kripa Shanker <kripa.shanker@kaleidoscope-blockchain.com>
2024-07-12 00:00:00 +0530

    integrate web3signer in register watchtower

--- common/constants.go
+++ common/constants.go
@@ -1,5 +1,11 @@
 package wc_common
 
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
 const (
 	DefaultAdminConfig   string  = "config/admin-config.json"
 	DefaultOpL1Config    string  = "config/l1-operator-config.json"
@@ -11,3 +17,30 @@ const (
 	MaxMountRetries      int     = 5
 	RetryPeriodInSeconds uint    = 1
 )
+
+type ChainConfig struct{
+	OperatorRegistryAddress common.Address
+	WitnessHubAddress       common.Address
+	AVSDirectoryAddress     common.Address
+	ChainID                 big.Int
+
+  // "operator_registry_address": "0xEf1a89841fd189ba28e780A977ca70eb1A5e985D",
+  // "witnesshub_address": "0xD25c2c5802198CB8541987b73A8db4c9BCaE5cC7",
+  // "avs_directory_address": "0x135dda560e946695d6f155dacafc6f1f25c1f5af",
+  // "eth_rpc_url": "https://blue-orangutan-rpc.eu-north-2.gateway.fm/",
+  // "chain_id": 1237146866,
+}
+
+var BlueOrangutan = ChainConfig {
+	OperatorRegistryAddress: common.HexToAddress("0x26710e60A36Ace8A44e1C3D7B33dc8B80eAb6cb7"),
+	WitnessHubAddress: common.HexToAddress("0xD25c2c5802198CB8541987b73A8db4c9BCaE5cC7"),
+	AVSDirectoryAddress: common.HexToAddress("0x135dda560e946695d6f155dacafc6f1f25c1f5af"),
+	ChainID: *big.NewInt(1237146866),
+}
+
+var NetworkConfig = map[string] ChainConfig {
+	"1237146866": BlueOrangutan,
+}
+
+
+
--- watchtower-operator/commands/signature.go
+++ watchtower-operator/commands/signature.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"math/big"
 
+	"github.com/witnesschain-com/diligencewatchtower-client/keystore"
 	wc_common "github.com/witnesschain-com/operator-cli/common"
 	"github.com/witnesschain-com/operator-cli/common/bindings/AvsDirectory"
 	"github.com/witnesschain-com/operator-cli/common/bindings/OperatorRegistry"
@@ -39,17 +40,9 @@ func GetOpertorSignature(client *ethclient.Client, avsDirectory *AvsDirectory.Av
 	return operatorSignature
 }
 
-func SignOperatorAddress(client *ethclient.Client, operatorRegistry *OperatorRegistry.OperatorRegistry, watchtowerPrivateKey *ecdsa.PrivateKey, operatorAddress common.Address, salt [32]byte, expiry *big.Int) []byte {
-	digestHash, err := operatorRegistry.CalculateWatchtowerRegistrationMessageHash(&bind.CallOpts{}, operatorAddress, salt, expiry)
-	wc_common.CheckError(err, "Digest hash calculation failed")
-
-	signature, err := crypto.Sign(digestHash[:], watchtowerPrivateKey)
-	wc_common.CheckError(err, "Signing the digest hash failed")
-
-	v := new(big.Int).SetBytes(signature[64:])
-	v.Add(v, big.NewInt(27))
-
-	// Construct the full signature (r, s, v)
-	fullSignature := append(signature[:64], v.Bytes()...)
+func SignOperatorAddress(client *ethclient.Client, operatorRegistry *OperatorRegistry.OperatorRegistry, vault *keystore.Vault, OperatorAddress common.Address, salt [32]byte, expiry big.Int) []byte {
+	digestHash, err := operatorRegistry.CalculateWatchtowerRegistrationMessageHash(&bind.CallOpts{}, OperatorAddress, salt, &expiry)
+	fullSignature, err := vault.SignData(digestHash[:])
+	wc_common.CheckError(err, "unable to sign operator address")
 	return fullSignature
 }
